4f820bcd7769e3533ce5cd7df256b0b9da72d018
Benjamin Jones <bljones36@gmail.com>
2019-02-07 02:47:49 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,7 +24,6 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id/edit' do
     @landmark = Landmark.find(params[:id])
-    binding.pry
     erb :'/landmarks/edit'
   end
 
