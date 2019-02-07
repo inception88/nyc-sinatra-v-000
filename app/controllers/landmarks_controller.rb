56a8ec10cca7f1aba730c6441530d7c490f1e5fa
Benjamin Jones <bljones36@gmail.com>
2019-02-07 02:20:28 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,7 +9,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    @landmark = Landmark.create(params[:landmark][:name])
+    @landmark = Landmark.create(name: params[:landmark][:name], year: params[:landmark][:year_completed])
   end
 
   get '/landmarks/:id' do
